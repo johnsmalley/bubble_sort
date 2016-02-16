92243b309abb313d70c03d06b772df33465cd13a
johnsmalley <john.m.smalley@gmail.com>
2016-02-15 20:21:23 -0600

    first part of bubble sort solved

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,1 +1,33 @@
-#Test
+=begin
+
+This program came from The Odin Project
+http://www.theodinproject.com/ruby-programming/advanced-building-blocks
+
+Build a method #bubble_sort that takes an array and returns a sorted array. 
+It must use the bubble sort methodology (using #sort would be pretty pointless, 
+wouldn't it?).
+	
+=end
+
+sample_array = [4,3,78,2,0,2]
+
+def bubble_sort(array)
+	switch = true
+	
+	while switch do # keep going through the array until no more switches needed
+		switch = false
+		(array.length - 1).times do |i|
+			if array[i] > array[i+1]
+				# switches the elements in the array to get to the right order
+				array[i+1],array[i] = array[i],array[i+1]
+				# flags that a switch occured, so need go through array again
+				switch = true 
+			end
+		end	
+	end
+
+	return array
+
+end
+
+p bubble_sort(sample_array)
